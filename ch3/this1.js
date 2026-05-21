9ec7b9a0b5df0e9c0a28f3b2667a327219017053
Simhyeok <simhyuck08@naver.com>
2026-05-21 20:56:01 +0900

    .

--- ch3/this1.js
+++
@@ -1,6 +0,0 @@
-//단순함수호출시 this
-
-const myfunction = function() {
-    return this
-}
-console.log(this)
--- ch3/this2.js
+++
@@ -1,9 +0,0 @@
-const myobj = {
-    numbers : [1,2,3],
-    myfunction : function() {
-        console.log(this)
-        this.numbers.forEach(0)
-    }
-}
-
-myobj.myfunction()
